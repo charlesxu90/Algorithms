3fce430c694f2f585fed15136218f393ffb4ac8f
charlesxu90 <charlesxu90@gmail.com>
2018-09-06 23:16:25 +0800

    add new *.ipynbfiles, replace old *.py file

--- MaxPoints.py
+++ MaxPoints.py
@@ -32,6 +32,7 @@ Explanation:
 
 """
 
+from decimal import *
 
 # Definition for a point.
 class Point(object):
@@ -47,30 +48,35 @@ class Solution(object):
         :rtype: int
         """
         size = len(points)
-        if size  < 3:
+        if size < 3:
             return size
         ans = 0
         for i in range(size):
-            d = {'inf':0}
-            samePoint = 1
+            d = {'inf': 0}
+            same_point = 1
             for j in range(size):
+                print("i:" +  str(points[i].x) + " " + str(points[i].y))
+                print("j:" + str(points[j].x) + " " + str(points[j].y))
+
                 if i == j:
                     continue
                 elif points[i].x == points[j].x and points[i].y != points[j].y:
                     d['inf'] += 1
                 elif points[i].x != points[j].x:
-                    k = 1.0 * (points[i].y - points[j].y) / (points[i].x - points[j].x)
+                    k = Decimal(points[i].y - points[j].y) / (points[i].x - points[j].x)
+                    print(k)
                     if k in d:
                         d[k] += 1
                     else:
                         d[k] = 1
                 else:
-                    samePoint += 1
-            ans = max(ans,max(d.values()) + samePoint)
+                    same_point += 1
+            ans = max(ans, max(d.values()) + same_point)
         return ans
 
 
+# [[0,0],[94911151,94911150],[94911152,94911151]]
 if __name__ == '__main__':
-    points = [Point(0, 0), Point(1, 1), Point(2, 2), Point(3, 1)]
+    points = [Point(0,0), Point(94911151,94911150), Point(94911152,94911151)]
     ret = Solution().maxPoints(points)
     print(ret)
